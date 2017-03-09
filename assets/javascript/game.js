c41c1a0c20b8f4599b4ad56a793637658348694b
ppreziotti <patpreziotti@gmail.com>
2017-03-08 20:17:19 -0500

    more updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,52 @@
 // Creates an array that will hold all of the words to be used in the game 
-var words = ["guitar", "bass", "drums", "keyboard", "synthesizer", "saxophone", "organ", "fiddle", "dobro", "harpsichord"];
-
-// Picks a random value from the words array to be used as the word for the current round 
-var randomWord = words[Math.floor(Math.random() * words.length)];
-
-console.log(randomWord);
-
-// Creates an empty array to be used for storing the chosen word 
-var chosenWord = [];
+var wordArray = ["guitar", "bass", "drums", "keyboard", "synthesizer", "saxophone", "organ", "fiddle", "dobro", "harpsichord"];
 
 // Creates variables that will display game information 
-// lettersGuessed is an empty array that guessed letters will be pushed to
+// lettersGuessed and chosenWord are empty arrays to which letters will be pushed
 var wins = 0;
 var guessesRemaining = 6;
 var lettersGuessed = [];
+var chosenWord = [];
 
-// Loops through the current randomWord and shows each letter as an underscore in the chosenWord array
-for (var i = 0; i < randomWord.length; i++) {
-	chosenWord[i] = "_";
+// Creates a function that picks a random word from wordsArray, puts guesses remaining back at 6 and clears letters guessed
+function newWord() {
+	
+	// Picks a random value from wordArray to be used as the word for the current round 
+	randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+	guessesRemaining = 6;
+	// Displays the amout of guesses remaining in the appropriate heading
+	document.getElementById("guesses-remaining").innerHTML = "Guesses Remaining: " + guessesRemaining;
+	lettersGuessed = [];
+	document.getElementById("letters-guessed").innerHTML = "Letters Guessed: ";
+
 }
 
-// Converts the chosenWord array to a string without commas
-var wordDisplay = chosenWord.join(" ");
-console.log(wordDisplay);
+function displayWord() {
 
-// Displays wordDisplay in the html within the word-space id
-document.getElementById("word-space").innerHTML = wordDisplay;
-document.getElementById("guesses-remaining").innerHTML = "Guesses Remaining: " + guessesRemaining;
+	// Clears the chosenWord array
+	chosenWord = [];
+	// Loops through the current randomWord and stores each letter as an underscore in the chosenWord array
+	for (var i = 0; i < randomWord.length; i++) {
+		chosenWord[i] = "_";
+	}
+	// Converts the chosenWord array to a string without commas and displays it in the word-space heading
+	document.getElementById("word-space").innerHTML = chosenWord.join(" ");
+	
+}
 
-// Creates a function to log the key the user presses and then runs the game's logic
+newWord();
+displayWord();
+
+// Creates a function that logs the key the user presses and then runs the game's logic
 document.onkeyup = function() {
 
+	// Pulls the letter from the user's key input and converts to lower case (if necessary)
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-	console.log(userGuess);
 
 	// Loops through the randomWord checking for the letter guessed by the user
-	for (var i = 0; i < randomWord.length; i++) {
-	
-		// If the user's guessed letter is in the word, it will replace the underscore in the chosenWord array and displayed on the page
+	for (var i = 0; i <= randomWord.length; i++) {
+
+		// If the user's guessed letter is in the word, it will replace the underscore in the chosenWord array and be displayed on the page
 		if (randomWord[i] === userGuess) {
 			chosenWord[i] = randomWord[i];
 			document.getElementById("word-space").innerHTML = chosenWord.join(" ");
@@ -45,16 +54,33 @@ document.onkeyup = function() {
 
 		// If the user's guessed letter is not in the word and has not already been guessed:
 		// guessesRemaining decreases by one, the guessed letter is pushed to the lettersGuessed array, and then displayed
-		else if (randomWord.indexOf(userGuess) === -1 && lettersGuessed.indexOf(userGuess) === -1) {
+		if (randomWord.indexOf(userGuess) === -1 && lettersGuessed.indexOf(userGuess) === -1) {
 			guessesRemaining--;
 			document.getElementById("guesses-remaining").innerHTML = "Guesses Remaining: " + guessesRemaining;
 			lettersGuessed.push(userGuess);
 			document.getElementById("letters-guessed").innerHTML = "Letters Guessed: " + lettersGuessed.toString();
 		}
 
+
+		// If the user guesses the word correctly (no underscores are left), wins increases by 1, a win alert is shown,
+		// and the win count is displayed
+		if (chosenWord.indexOf("_") === -1) {
+			wins++;
+			alert("You guessed the word correctly! Press OK to star a new round.");
+			document.getElementById("win-count").innerHTML = "Wins: " + wins;
+			newWord();
+			displayWord();
+		}
+
+		// If the user runs out of guesses, the game ends and a new randomWord is generated
+		if (guessesRemaining === 0) {
+			alert("You ran out of guesses. Press OK to start a new round.");
+			newWord();
+			displayWord();
+		}
+
 	}
 
-	//console.log(guessesRemaining);
-	//console.log(lettersGuessed);
+	
 
 }
